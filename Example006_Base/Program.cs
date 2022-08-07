756fc1e6d805e82cdb72349125236f84f2676ac4
rybkin <erybkin@yandex.ru>
2022-08-07 12:46:16 +0700

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 91;
+int b = 11;
+int c = 21;
+int d = 13;
+int e = 41;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
