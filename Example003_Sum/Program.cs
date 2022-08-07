bb761ed562e496b3485cb5432880765f3122d108
rybkin <erybkin@yandex.ru>
2022-08-07 12:36:44 +0700

    add example004 and example005

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int number1 = 3;
-int number2 = 5;
+﻿int number1 = new Random().Next(1, 10);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 Console.WriteLine(number1 + number2);
---
+++ Example004_Div/Program.cs
@@ -0,0 +1,3 @@
+﻿double a = 10;
+double b = 4;
+Console.WriteLine(a / b);
