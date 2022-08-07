1633e6dd22459d646c93fb18b4db9adbfc42fcdc
rybkin <erybkin@yandex.ru>
2022-08-07 11:51:53 +0700

    add Example002_Hello_user

--- Example001-Hello console/Program.cs
+++ Example001-Hello console/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string username = Console.ReadLine();
+Console.WriteLine("Hello!");
+Console.WriteLine(username);
+
