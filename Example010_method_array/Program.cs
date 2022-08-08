19524562a6462de920d0d62597d316a0dbc84cc7
rybkin <erybkin@yandex.ru>
2022-08-08 20:46:25 +0700

    add Example010_method_array

---
+++ Example010_method_array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {121, 14, 211, 561, 561, 431, 137, 1879, 17875};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == find);
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
